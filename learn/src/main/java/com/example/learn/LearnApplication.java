f760b748440eb979b52c54d62d62b225acc3cb56
pankajan05 <pankajan05@gmail.com>
2020-08-11 11:19:13 +0530

    create spring boot web application

--- learn/src/main/java/com/example/learn/LearnApplication.java
+++ learn/src/main/java/com/example/learn/LearnApplication.java
@@ -1,13 +1,21 @@
 package com.example.learn;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class LearnApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(LearnApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner runner(SimpleProperties simpleProperties, NestedProperties nestedProperties) {
+            return r -> {
+            };
     }
 
 }
